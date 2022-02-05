10ceb1a3f7bffa0bf5e820bdcc0b3035ff9c03f8
3a1e4b <3a1e4b@gmail.com>
2022-02-05 07:47:58 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+        console.log('к серверу обратился какой то уебок')
+        res.setHeader('Content-Type', 'text/html;charset=utf-8')
+//      res.setHeader('charset', 'utf-8')
+        res.write('<h1>Здорово хуила</h1><form><button autofocus>Ёбни по мне</button></form>')
+        res.end()
+});
+
+const PORT = 3000;
+server.listen(PORT, 'localhost', (error) => {
+        error ? console.log(error) : console.log(`listen on ${PORT}`)                                  
+});
