6dec095efbc479c1e705faccc3389e907d566dc2
3a1e4b <3a1e4b@gmail.com>
2022-05-13 19:59:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-        console.log('к серверу обратился какой то уебок')
+        console.log('к серверу обратился какой то ...')
         res.setHeader('Content-Type', 'text/html;charset=utf-8')
 //      res.setHeader('charset', 'utf-8')
-        res.write('<h1>Здорово хуила</h1><form><button autofocus>Ёбни по мне</button></form>')
+        res.write('<h1>Здорово, ...!</h1><form><button autofocus>... по мне</button></form>')
         res.end()
 });
 
